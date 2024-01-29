89e7d8aca6ace598cde0507781efc4d685a11618
Beeranna Dude <101712802+beeru405@users.noreply.github.com>
2024-01-29 14:55:02 +0530

    Update Login.java

--- src/it/java/test/stepdef/Login.java
+++ src/it/java/test/stepdef/Login.java
@@ -19,7 +19,7 @@ public class Login{
 	
 	@Before
 	public void init(){
-		System.setProperty("webdriver.chrome.driver", "D:/Documents/Downloads/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
